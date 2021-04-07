98e8216d4d8e6ef06f259a4fb99d73ea19b71916
Amal V Nair <amalvnair95@gmail.com>
2021-04-07 21:12:57 +0530

    Admin panel + missing imports

--- scheduler_schema/admin.py
+++ scheduler_schema/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Schedule
 # Register your models here.
+
+class ScheduleAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', )
+    list_display = ('__str__', 'id', 'title', 'date_time',)
+
+admin.site.register(Schedule, ScheduleAdmin)
